f317c603e0d441ade984eb74df4ee8cc6af50c70
Kafulkaff99 <kafulkaff02@gmail.com>
2024-10-22 01:38:55 +0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+import os
+
+print("""[0] pip\n[1] pip3\nWhich one do you use?""")
+
+c = input(">>>: ")
+if c == "0":
+    os.system("pip install cloudscraper")
+    os.system("pip install socks")
+    os.system("pip install pysocks")
+    os.system("pip install colorama")
+    
+
+elif c == "1":
+    os.system("pip3 install cloudscraper")
+    os.system("pip3 install socks")
+    os.system("pip3 install pysocks")
+    os.system("pip3 install colorama")
+    
+
+print("Done.")
